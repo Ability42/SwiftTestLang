5407e1bf9affa8ec529931a342ad2af94bb9586f
Stepan Paholyk <stepanpaholyk@gmail.com>
2017-04-15 19:52:11 +0300

    Add 'generics' topic

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -105,7 +105,7 @@ extension Queue {
         guard let first = elements.first else {
             return true
         }
-        return elements.contains{ $0 == first }
+        return !elements.contains{ $0 != first }
     }
     
 }
@@ -190,5 +190,179 @@ func divide(_ x: Int, by y: Int) -> Result<Int> {
 
 let resultWithoutFailure = divide(32, by: 4)
 let resultWithFailure = divide(42, by: 0)
+
+
+//: Generics topic from developer.apple.com
+
+func swapTwoValues<T>(_  a: inout T, _ b: inout T) {
+    let tempA = a
+    a = b
+    b = tempA
+}
+
+var a = 4
+var b = 0
+
+swapTwoValues(&a, &b)
+print("a is \(a), b is \(b)")
+
+
+//: Type constraint in action
+
+func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
+    for (index, value) in array.enumerated() {
+        if value == valueToFind {
+            return index
+        }
+    }
+    return nil
+}
+
+//: Associated Types in Action
+
+protocol Container {
+    associatedtype Item
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+
+struct IntStack: Container {
+    
+    // original IntStack implementation
+    var items = [Int]()
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+    
+    // conformance to Container protocol
+    
+    typealias Item = Int
+    
+    mutating func append(_ item: Int) {
+        self.push(item)
+    }
+    
+    var count: Int {
+        return items.count
+    }
+    
+    subscript(i: Int) -> Int {
+        return items[i]
+    }
+    
+}
+
+struct Stack<Element>: Container {
+    // original Stack<Element> implementation
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+    // conformance to the Container protocol
+    mutating func append(_ item: Element) {
+        self.push(item)
+    }
+    var count: Int {
+        return items.count
+    }
+    subscript(i: Int) -> Element {
+        return items[i]
+    }
+}
+
+extension Array : Container {}
+
+//: Generic Where Clauses
+
+
+func allItemsMatch<C1: Container, C2: Container>(_ someContainer: C1, _ anotherContainer: C2) -> Bool where C1.Item == C2.Item, C1.Item: Equatable {
+    
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+    
+    for i in 0..<someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+    
+    return true
+}
+
+
+var stackOfStrings = Stack<String>()
+
+stackOfStrings.append("uno")
+stackOfStrings.append("dos")
+stackOfStrings.append("tres")
+
+var arrayOfStrings = ["uno", "dos", "tres"]
+
+if allItemsMatch(stackOfStrings, arrayOfStrings) {
+    print("All items match.")
+} else {
+    print("Not all items match.")
+}
+
+
+//: Extensions with a Generic Where Clause
+
+extension Stack where Element: Equatable {
+    func isTop(_ item: Element) -> Bool {
+        guard let topItem = items.last else {
+            return false
+        }
+        return topItem == item
+    }
+}
+
+if stackOfStrings.isTop("tres") {
+    print("Top element is tres.")
+} else {
+    print("Top element is something else.")
+}
+
+struct NotEquatable { }
+
+var notEquatableStack = Stack<NotEquatable>()
+let notEquatableValue = NotEquatable()
+notEquatableStack.push(notEquatableValue)
+// notEquatableStack.isTop(notEquatableStack) // error
+
+extension Container where Item: Equatable {
+    func startsWith(_ item: Item) -> Bool {
+        return count >= 1 && self[0] == item
+    }
+}
+
+if [4,2,1,0].startsWith(4) {
+    print("Strarts with 4")
+} else {
+    print("Starts with something else")
+}
+
+extension Container where Item == Double {
+    func average() -> Double {
+        var sum = 0.0
+        for index in 0..<count {
+            sum += self[index]
+        }
+        return sum/Double(count)
+    }
+}
+
+print([4.6, 5.6, 44.2].average())
+
+
+
 
 
